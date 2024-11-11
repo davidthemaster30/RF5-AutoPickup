71d53c03c9b02a96138155ab0751132a10a1938b
David <davidthemaster30@hotmail.com>
2024-11-10 22:13:09 -0500

    Extract Known Enums

---
+++ KnownItemIDs.cs
@@ -0,0 +1,39 @@
+namespace RF5AutoPickup;
+
+internal static class KnownItemIDs
+{
+    internal static readonly ItemID[] ItemIDGrassesOnGroundToAutoPickupEng = [
+        ItemID.Item_Grass_0001, //Indigo Grass
+        ItemID.Item_Grass_0002, //Blue Grass
+        ItemID.Item_Grass_0006, //Elli Leaves
+        ItemID.Item_Grass_0014, //Yellow Grass
+        ItemID.Item_Grass_0018, //Black Grass
+        ItemID.Item_Grass_0021, //Weeds
+        ItemID.Item_Grass_0023, //White Grass
+        ItemID.Item_Grass_0026, //Orange Grass
+        ItemID.Item_Grass_0037, //Antidote Grass
+        ItemID.Item_Grass_0042, //Green Grass
+        ItemID.Item_Grass_0044, //Purple Grass
+        ItemID.Item_Grass_0045  //Medicinal Herb
+    ];
+
+    internal static readonly ItemID[] ItemIDGrassesOnGroundToAutoPickupJap = [
+        ItemID.Item_Ainokusa, //Indigo Grass
+        ItemID.Item_Aoikusa, //Blue Grass
+        ItemID.Item_Erikusa, //Elli Leaves
+        ItemID.Item_Kinokusa, //Yellow Grass
+        ItemID.Item_Kuroikusa, //Black Grass
+        ItemID.Item_Zasso, //Weeds
+        ItemID.Item_Shiroikusa, //White Grass
+        ItemID.Item_Daidaikusa, //Orange Grass
+        ItemID.Item_Dokukeshikusa, //Antidote Grass
+        ItemID.Item_Midorinokusa, //Green Grass
+        ItemID.Item_Murasakinokusa, //Purple Grass
+        ItemID.Item_Yakuso  //Medicinal Herb
+    ];
+
+    internal const ItemID WitheredGrassItemID = ItemID.Item_Grass_0013; //Withered Grass
+    internal const ItemID RockItemID = ItemID.Item_Collection_0175; //Rock
+    internal const ItemID BranchItemID = ItemID.Item_Collection_0176; //Branch
+    internal const ItemID CornItemID = ItemID.Item_Crop_0034; //Corn
+}
---
+++ KnownPrefabs.cs
@@ -0,0 +1,26 @@
+using Loader.ID;
+
+namespace RF5AutoPickup;
+
+internal static class KnownPrefabs
+{
+    internal static readonly Prefab[] PrefabGrassesOnGroundToAutoPickup = [
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0001, //Indigo Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0002, //Blue Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0006, //Elli Leaves
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0014, //Yellow Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0018, //Black Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0021, //Weeds
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0023, //White Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0026, //Orange Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0037, //Antidote Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0042, //Green Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0044, //Purple Grass
+        Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0045  //Medicinal Herb
+    ];
+
+    internal const Prefab WitheredGrassPrefab = Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_GRASS_0013; //Withered Grass
+    internal const Prefab RockPrefab = Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_COLLECTION_0175; //Rock
+    internal const Prefab BranchPrefab = Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_COLLECTION_0176; //Branch
+    internal const Prefab CornPrefab = Prefab.ITEM_ONGROUNDITEM_ONGROUNDITEM_ITEM_CROP_0034; //Corn
+}
