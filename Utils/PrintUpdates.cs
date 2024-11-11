2ebd2d57abc6470ab2bdeb1a66934a1f411222f3
David <davidthemaster30@hotmail.com>
2024-11-10 22:14:15 -0500

    Add more print functions

--- Utils/PrintUpdates.cs
+++ Utils/PrintUpdates.cs
@@ -1,4 +1,5 @@
 using BepInEx.Logging;
+using Loader.ID;
 
 namespace RF5AutoPickup.Print;
 
@@ -8,9 +9,39 @@ internal static class PrintUpdates
 
     internal static void ShowItemUpdate(ItemDataTable item)
     {
-        if (AutoPickup.EnableLogging?.Value == true && Log is not null && item is not null && !string.IsNullOrWhiteSpace(item.ScreenName))
+        if (AutoPickup.EnableLogging?.Value == true && item is not null && !string.IsNullOrWhiteSpace(item.ScreenName))
         {
             Log.LogInfo($"{item.ScreenName} has been updated to {(item.IsAutoPickup ? "enable" : "disable")} auto pickup!");
         }
+    }
+
+    internal static void PrintException(Exception ex)
+    {
+        if (AutoPickup.EnableLogging?.Value == true && Log is not null)
+        {
+            Log.LogError(ex);
+        }
+    }
+    internal static void PrintRemainingItems(Dictionary<ItemID, bool> ItemsToUpdate)
+    {
+        Log.LogInfo($"Printing list of remaining items.");
+        foreach (var item in ItemsToUpdate.Where(x => !x.Value))
+        {
+            Log.LogInfo($"{item} not yet patched for auto pickup.");
+        }
+    }
+
+    internal static void PrintRemainingItems(Dictionary<Prefab, bool> ItemsToUpdate)
+    {
+        Log.LogInfo($"Printing list of remaining items.");
+        foreach (var item in ItemsToUpdate.Where(x => !x.Value))
+        {
+            Log.LogInfo($"{item} not yet patched for auto pickup.");
+        }
+    }
+
+    internal static void AllItemsPatched()
+    {
+        Log.LogInfo("Alls items patched for auto pickup.");
     }
 }
