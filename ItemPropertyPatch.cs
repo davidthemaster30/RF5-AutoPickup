1aaed377bc1ec1389d38da1126081f67fa4a2366
David <davidthemaster30@hotmail.com>
2024-11-21 00:41:18 -0500

    Make mod compatible with Configuration Manager

--- ItemPropertyPatch.cs
+++ ItemPropertyPatch.cs
@@ -1,3 +1,4 @@
+using BepInEx.Configuration;
 using HarmonyLib;
 using Loader.ID;
 using RF5AutoPickup.Print;
@@ -9,41 +10,44 @@ internal static class ItemPropertyPatch
 {
     internal static readonly Dictionary<ItemID, bool> ItemsToUpdate = new Dictionary<ItemID, bool>();
 
-    internal static readonly Dictionary<Prefab, bool> PrefabsToUpdate = new Dictionary<Prefab, bool>();
-
     internal static bool isPatched { get; set; } = false;
+
+    internal static void OnSettingChanged(object sender, EventArgs e)
+    {
+        var eventArgs = e as SettingChangedEventArgs;
+        PrintUpdates.PrintSettingChanged(sender, eventArgs);
+
+        if (ItemDataTable._ItemDataTableArray?.DataTables is null)
+        {
+            //Change was before data was loaded (on title screen?)
+            return;
+        }
+
+        isPatched = false;
+        //force update toggle on all
+        foreach (var key in ItemsToUpdate.Keys)
+        {
+            ItemsToUpdate[key] = false;
+        }
+
+        //force update
+        _ = ItemDataTable.GetDataTable(ItemsToUpdate.First().Key);
+    }
 
     internal static void Prepare()
     {
         if (ItemsToUpdate.Count == 0)
         {
-            if (AutoPickup.EnableAutoPickupGrasses.Value)
+            //First pass has items IDs added according to initial config
+            foreach (var item in KnownItemIDs.ItemIDGrassesOnGroundToAutoPickupEng)
             {
-                foreach (var item in KnownItemIDs.ItemIDGrassesOnGroundToAutoPickupEng)
-                {
-                    ItemsToUpdate.Add(item, false);
-                }
+                ItemsToUpdate.Add(item, AutoPickup.EnableAutoPickupGrasses.Value);
             }
 
-            if (AutoPickup.EnableAutoPickupWitheredGrass.Value)
-            {
-                ItemsToUpdate.Add(KnownItemIDs.WitheredGrassItemID, false);
-            }
-
-            if (AutoPickup.EnableAutoPickupRocks.Value)
-            {
-                ItemsToUpdate.Add(KnownItemIDs.RockItemID, false);
-            }
-
-            if (AutoPickup.EnableAutoPickupBranches.Value)
-            {
-                ItemsToUpdate.Add(KnownItemIDs.BranchItemID, false);
-            }
-
-            if (AutoPickup.DisableAutoPickupCorn.Value)
-            {
-                ItemsToUpdate.Add(KnownItemIDs.CornItemID, false);
-            }
+            ItemsToUpdate.Add(KnownItemIDs.WitheredGrassItemID, AutoPickup.EnableAutoPickupWitheredGrass.Value);
+            ItemsToUpdate.Add(KnownItemIDs.RockItemID, AutoPickup.EnableAutoPickupRocks.Value);
+            ItemsToUpdate.Add(KnownItemIDs.BranchItemID, AutoPickup.EnableAutoPickupBranches.Value);
+            ItemsToUpdate.Add(KnownItemIDs.CornItemID, AutoPickup.DisableAutoPickupCorn.Value);
         }
     }
 
@@ -58,39 +62,39 @@ internal static class ItemPropertyPatch
             return;
         }
 
-        if (AutoPickup.EnableAutoPickupGrasses.Value && KnownItemIDs.ItemIDGrassesOnGroundToAutoPickupEng.Contains(itemID))
+        if (KnownItemIDs.ItemIDGrassesOnGroundToAutoPickupEng.Contains(itemID))
         {
-            TrackUpdate(itemID);
-            __result.IsAutoPickup = true;
+            __result.IsAutoPickup = AutoPickup.EnableAutoPickupGrasses.Value;
             PrintUpdates.ShowItemUpdate(__result);
+            TrackUpdate(itemID);
         }
 
-        if (AutoPickup.EnableAutoPickupWitheredGrass.Value && itemID == KnownItemIDs.WitheredGrassItemID)
+        if (itemID == KnownItemIDs.WitheredGrassItemID)
         {
-            TrackUpdate(itemID);
-            __result.IsAutoPickup = true;
+            __result.IsAutoPickup = AutoPickup.EnableAutoPickupWitheredGrass.Value;
             PrintUpdates.ShowItemUpdate(__result);
+            TrackUpdate(itemID);
         }
 
-        if (AutoPickup.EnableAutoPickupRocks.Value && itemID == KnownItemIDs.RockItemID)
+        if (itemID == KnownItemIDs.RockItemID)
         {
-            TrackUpdate(itemID);
-            __result.IsAutoPickup = true;
+            __result.IsAutoPickup = AutoPickup.EnableAutoPickupRocks.Value;
             PrintUpdates.ShowItemUpdate(__result);
+            TrackUpdate(itemID);
         }
 
-        if (AutoPickup.EnableAutoPickupBranches.Value && itemID == KnownItemIDs.BranchItemID)
+        if (itemID == KnownItemIDs.BranchItemID)
         {
-            TrackUpdate(itemID);
-            __result.IsAutoPickup = true;
+            __result.IsAutoPickup = AutoPickup.EnableAutoPickupBranches.Value;
             PrintUpdates.ShowItemUpdate(__result);
+            TrackUpdate(itemID);
         }
 
-        if (AutoPickup.DisableAutoPickupCorn.Value && itemID == KnownItemIDs.CornItemID)
+        if (itemID == KnownItemIDs.CornItemID)
         {
-            TrackUpdate(itemID);
-            __result.IsAutoPickup = false;
+            __result.IsAutoPickup = !AutoPickup.DisableAutoPickupCorn.Value;
             PrintUpdates.ShowItemUpdate(__result);
+            TrackUpdate(itemID);
         }
 
         //force immediate update for next item
@@ -98,7 +102,6 @@ internal static class ItemPropertyPatch
         var item = ItemsToUpdate.FirstOrDefault(x => !x.Value);
         if (item.Key != default)
         {
-            PrintUpdates.Log.LogMessage($"force update of {item}");
             _ = ItemDataTable.GetDataTable(item.Key);
         }
     }
@@ -114,6 +117,7 @@ internal static class ItemPropertyPatch
         {
             isPatched = true;
             PrintUpdates.AllItemsPatched();
+            PrintDatatypes.PrintDatatables(ItemDataTable._ItemDataTableArray.DataTables);
         }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -43,22 +43,30 @@ public class AutoPickup : BasePlugin
         DisableAutoPickupCorn is not null && DisableAutoPickupCorn.Value;
     }
 
+    internal static void LoadConfig(ConfigFile Config){
+        EnableLogging = Config.Bind("General", nameof(EnableLogging), true, "Set to true to enable logging of changed values by the mod.");
+
+        EnableAutoPickupGrasses = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupGrasses), true, "Set to true to enable auto pickup grasses.");
+        EnableAutoPickupGrasses.SettingChanged += ItemPropertyPatch.OnSettingChanged;
+        EnableAutoPickupRocks = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupRocks), false, "Set to true to enable auto pickup stone.");
+        EnableAutoPickupRocks.SettingChanged += ItemPropertyPatch.OnSettingChanged;
+        EnableAutoPickupBranches = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupBranches), false, "Set to true to enable auto pickup lumber.");
+        EnableAutoPickupBranches.SettingChanged += ItemPropertyPatch.OnSettingChanged;
+        EnableAutoPickupWitheredGrass = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupWitheredGrass), true, "Set to true to enable auto pickup withered grass.");
+        EnableAutoPickupWitheredGrass.SettingChanged += ItemPropertyPatch.OnSettingChanged;
+        DisableAutoPickupCorn = Config.Bind(PluginConfigSection, nameof(DisableAutoPickupCorn), false, "Set to true to disable auto pickup of corn.");
+        DisableAutoPickupCorn.SettingChanged += ItemPropertyPatch.OnSettingChanged;
+
+        EnableDumpItemDataTablesLogging = Config.Bind("Debug", nameof(EnableDumpItemDataTablesLogging), false, "Set to true output itemdatatables to log for mod developpement (NOT RECOMMENDED).");
+    }
+
     public override void Load()
     {
         // Plugin startup logic
         Log.LogInfo($"Plugin {PluginName} is loaded!");
 
         // Config
-        EnableLogging = Config.Bind("General", nameof(EnableLogging), true, "Set to true to enable logging of changed values by the mod.");
-
-        EnableAutoPickupGrasses = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupGrasses), true, "Set to true to enable auto pickup grasses.");
-        EnableAutoPickupRocks = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupRocks), false, "Set to true to enable auto pickup stone.");
-        EnableAutoPickupBranches = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupBranches), false, "Set to true to enable auto pickup lumber.");
-        EnableAutoPickupWitheredGrass = Config.Bind(PluginConfigSection, nameof(EnableAutoPickupWitheredGrass), true, "Set to true to enable auto pickup withered grass.");
-        DisableAutoPickupCorn = Config.Bind(PluginConfigSection, nameof(DisableAutoPickupCorn), false, "Set to true to disable auto pickup of corn.");
-
-        EnableDumpItemDataTablesLogging = Config.Bind("Debug", nameof(EnableDumpItemDataTablesLogging), false, "Set to true output itemdatatables to log for mod developpement (NOT RECOMMENDED).");
-
+        LoadConfig(Config);
 
         Harmony.CreateAndPatchAll(typeof(ItemPropertyPatch));
     }
