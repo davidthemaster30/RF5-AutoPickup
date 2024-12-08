9011a83bb26f112fc97312ad6397b19b3f121c4a
David <davidthemaster30@hotmail.com>
2024-12-08 18:42:13 -0500

    Bump version

--- make_release.sh
+++ make_release.sh
@@ -3,4 +3,4 @@ rm -rf bin
 
 dotnet build RF5AutoPickup.csproj -f net6.0 -c Release
 
-zip -j 'RF5AutoPickup_v1.2.0.zip' './bin/Release/net6.0/RF5AutoPickup.dll' './bin/Release/net6.0/RF5AutoPickup.cfg'
+zip -j 'RF5AutoPickup_v1.2.1.zip' './bin/Release/net6.0/RF5AutoPickup.dll' './bin/Release/net6.0/RF5AutoPickup.cfg'
